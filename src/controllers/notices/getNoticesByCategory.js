5b5b2538da8bccfdcb93368d6562d23bd451910a
arplaaay <artemb0607@gmail.com>
2023-03-18 17:17:18 +0200

    added notices/getNoticesByCategory

--- src/controllers/notices/getNoticesByCategory.js
+++ src/controllers/notices/getNoticesByCategory.js
@@ -1,7 +1,19 @@
+const { Notice } = require("../../schemas/notice");
+
 async function getNoticesByCategory(req, res) {
-  return res
-    .status(504)
-    .json({ message: "getNoticesByCategory not implemented" });
+  try {
+    const { category } = req.params;
+
+    const result = await Notice.find({ category });
+
+    if (!result || result.length === 0) {
+      return res.json({ message: "Not found" });
+    }
+
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.codeName });
+  }
 }
 
 module.exports = getNoticesByCategory;
