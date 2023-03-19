3a3234668b2327072a3f1140a31e83142b63e046
JaRcomGitHub <jarcom@ukr.net>
2023-03-19 18:41:23 +0200

    fix

--- src/controllers/userAndPets/index.js
+++ src/controllers/userAndPets/index.js
@@ -13,8 +13,14 @@ async function userAndPets(req, res) {
     photoURL: 1,
   });
 
-  const searchParam = { id };
-  const pets = await Pet.find(searchParam);
+  const searchParam = { owner: id };
+  const pets = await Pet.find(searchParam).select({
+    name: 1,
+    date: 1,
+    breed: 1,
+    comments: 1,
+    photoURL: 1,
+  });
 
   return res.status(200).json({ user: user, pets: pets });
 }
