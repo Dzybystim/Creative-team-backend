a5864094ff0730f73c6dcdc98001dff292e047e7
JaRcomGitHub <jarcom@ukr.net>
2023-03-19 18:12:05 +0200

    Update index.js

--- src/controllers/userAndPets/index.js
+++ src/controllers/userAndPets/index.js
@@ -1,5 +1,22 @@
+const { Pet } = require("../../schemas/pet");
+const { User } = require("../../schemas/user");
+
 async function userAndPets(req, res) {
-  return res.status(504).json({ message: "user + pets not implemented" });
+  const { id } = req.user;
+
+  const user = await User.findById(id).select({
+    _id: 0,
+    name: 1,
+    cityRegion: 1,
+    mobilePhone: 1,
+    birthdate: 1,
+    photoURL: 1,
+  });
+
+  const searchParam = { id };
+  const pets = await Pet.find(searchParam);
+
+  return res.status(200).json({ user: user, pets: pets });
 }
 
 module.exports = {
