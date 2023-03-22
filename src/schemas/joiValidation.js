484d814bd19e73863fb43bcadbaa63b4fc9d6f90
Anna Kovalchuk <annazukh@gmail.com>
2023-03-22 21:07:11 +0200

    joi-validation

--- src/schemas/joiValidation.js
+++ src/schemas/joiValidation.js
@@ -1,11 +1,14 @@
 const Joi = require("joi");
 
 const authSchema = Joi.object({
-  email: Joi.string().alphanum().min(5).email({ minDomainSegments: 2 }),
+  email: Joi.string()
+    .email({ minDomainSegments: 2 })
+    .pattern(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$/)
+    .min(5),
   password: Joi.string().alphanum().min(7).max(32),
-  name: Joi.string(),
+  name: Joi.string().pattern(/^[\p{L}\p{M}_-]+$/u),
   cityRegion: Joi.string(),
-  mobilePhone: Joi.string(),
+  mobilePhone: Joi.string().pattern(/^\+380\d{9}$/),
   birthdate: Joi.date(),
   photoURL: Joi.string(),
   selected: Joi.array(),
