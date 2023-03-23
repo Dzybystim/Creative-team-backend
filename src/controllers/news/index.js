36d0076807c72ec5e5dbb35709d326315c9ad5c0
JaRcomGitHub <jarcom@ukr.net>
2023-03-23 20:36:14 +0200

    Update index.js

--- src/controllers/news/index.js
+++ src/controllers/news/index.js
@@ -1,21 +1,17 @@
 const { New } = require("../../schemas/news");
 
-async function news(req, res, next) {
-  try {
-    const query = req.query.search;
+async function news(req, res) {
+  const query = req.query.search;
 
-    if (query) {
-      const searchResult = await New.find({
-        title: { $regex: query, $options: "i" },
-      });
-      return res.status(200).json(searchResult);
-    }
-
-    const result = await New.find({});
-    return res.status(200).json(result);
-  } catch (error) {
-    next(error);
+  if (query) {
+    const searchResult = await New.find({
+      title: { $regex: query, $options: "i" },
+    });
+    return res.status(200).json(searchResult);
   }
+
+  const result = await New.find({});
+  return res.status(200).json(result);
 }
 
 module.exports = {
