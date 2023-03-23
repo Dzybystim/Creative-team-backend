88bb597e666fe90e07485373d3b86068337ee0a8
JaRcomGitHub <jarcom@ukr.net>
2023-03-23 20:37:57 +0200

    Update index.js

--- src/controllers/servicesSidebar/index.js
+++ src/controllers/servicesSidebar/index.js
@@ -1,12 +1,8 @@
 const { servicesSideBar } = require("../../schemas/sideBar");
 
-async function servicesSidebar(req, res, next) {
-  try {
-    const searchResult = await servicesSideBar.find({});
-    return res.status(200).json(searchResult);
-  } catch (error) {
-    next(error);
-  }
+async function servicesSidebar(req, res) {
+  const searchResult = await servicesSideBar.find({});
+  return res.status(200).json(searchResult);
 }
 
 module.exports = {
