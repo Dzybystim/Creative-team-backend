7e1c45c3b21a382be878bdfe2138dd24cb14bae0
JaRcomGitHub <jarcom@ukr.net>
2023-03-23 20:36:47 +0200

    Update getNotice.js

--- src/controllers/notices/getNotice.js
+++ src/controllers/notices/getNotice.js
@@ -1,21 +1,16 @@
 const { Notice } = require("../../schemas/notice");
 
 async function getNotice(req, res) {
-  try {
-    const { noticeId } = req.params;
+  const { noticeId } = req.params;
 
-    const result = await Notice.findById(noticeId);
+  const result = await Notice.findById(noticeId);
 
-    if (!result) {
-      res.status(404);
-      throw new Error("Not found!");
-    }
-
-    res.json(result);
-  } catch (error) {
-    res.status(500);
-    throw new Error(error);
+  if (!result) {
+    res.status(404);
+    throw new Error("Not found");
   }
+
+  return res.status(200).json(result);
 }
 
 module.exports = getNotice;
