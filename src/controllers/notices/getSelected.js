efccb4182764c25f582a4c7b5c8f4461ac32e2e7
JaRcomGitHub <jarcom@ukr.net>
2023-03-30 19:55:16 +0300

    Update getSelected.js

--- src/controllers/notices/getSelected.js
+++ src/controllers/notices/getSelected.js
@@ -3,16 +3,20 @@ const { Notice } = require("../../schemas/notice");
 async function getSelected(req, res) {
   const { selected } = req.user;
 
-  const result = await Notice.find({}).where("_id").in(selected).select({
-    category: 1,
-    title: 1,
-    breed: 1,
-    location: 1,
-    birthdate: 1,
-    imageURL: 1,
-    price: 1,
-    owner: 1,
-  });
+  const result = await Notice.find({})
+    .where("_id")
+    .in(selected)
+    .select({
+      category: 1,
+      title: 1,
+      breed: 1,
+      location: 1,
+      birthdate: 1,
+      imageURL: 1,
+      price: 1,
+      owner: 1,
+    })
+    .sort({ updatedAt: -1 });
 
   return res.status(200).json(result);
 }
