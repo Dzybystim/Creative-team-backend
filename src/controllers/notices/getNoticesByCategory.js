3c19f04069d731043b841ea4e1bb99e82c7daf75
JaRcomGitHub <jarcom@ukr.net>
2023-03-30 19:55:13 +0300

    Update getNoticesByCategory.js

--- src/controllers/notices/getNoticesByCategory.js
+++ src/controllers/notices/getNoticesByCategory.js
@@ -3,7 +3,7 @@ const { Notice } = require("../../schemas/notice");
 async function getNoticesByCategory(req, res) {
   const query = req.query.category;
 
-  const result = await Notice.find({ category: query });
+  const result = await Notice.find({ category: query }).sort({ updatedAt: -1 });
 
   return res.status(200).json(result);
 }
