f7c6c02fc7b642b6c5265406da3f018118e0f2fc
JaRcomGitHub <jarcom@ukr.net>
2023-03-30 19:55:19 +0300

    Update getUserNotices.js

--- src/controllers/notices/getUserNotices.js
+++ src/controllers/notices/getUserNotices.js
@@ -3,7 +3,7 @@ const { Notice } = require("../../schemas/notice");
 async function getUserNotices(req, res) {
   const { _id: owner } = req.user;
 
-  const userNotices = await Notice.find({ owner });
+  const userNotices = await Notice.find({ owner }).sort({ updatedAt: -1 });
 
   return res.status(200).json(userNotices);
 }
